cfb3b7f67d520f2e33d89ff038cca15baa8887e9
NatiG <nati.g5684@gmail.com>
2022-07-30 15:36:16 +0300

    Remove pry

--- test.rb
+++ test.rb
@@ -1,4 +1,3 @@
-require 'pry'
 a = [1,2,3]
 b = [8,9,10,5]
 
@@ -6,7 +5,6 @@ a.map do |match|
     if match == 9
         puts "found a match"
     else
-        binding.pry
         puts "Not found"
     end
 end
